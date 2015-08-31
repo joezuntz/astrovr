084a457e485a811448c233040eff46eaa023f71b
Joe Zuntz <joezuntz@googlemail.com>
2015-08-31 12:04:51 +0100

    main back to healpix

--- main.cc
+++ main.cc
@@ -13,24 +13,24 @@ int main(int argc, char * argv[])
 
 
     // Set up the Healpix map object
-    // int nside = 128;
-    // float radius = 0.9f;
-    // AVRHealpix * hmap = new AVRHealpix(nside, radius);
-    // hmap->load("map.fits");
-    // hmap->createProgram("shaders/healpix/vertex.shader", "shaders/healpix/fragment.shader");
+    int nside = 128;
+    float radius = 0.9f;
+    AVRHealpix * hmap = new AVRHealpix(nside, radius);
+    hmap->load("map.fits");
+    hmap->createProgram("shaders/healpix/vertex.shader", "shaders/healpix/fragment.shader");
 
     // AVRCatalog * catalog = new AVRCatalog();
 	// catalog->load("cat.fits");
 	// catalog->createProgram("shaders/catalog/vertex.shader", "shaders/catalog/fragment.shader");
 
-    AVRImage * img = new AVRImage();
-    img->createProgram("shaders/image/vertex.shader", "shaders/image/fragment.shader");
-    img->setupImage("gal.fits");
-    checkGLerror("after load_fits");
+    // AVRImage * img = new AVRImage();
+    // img->createProgram("shaders/image/vertex.shader", "shaders/image/fragment.shader");
+    // img->setupImage("gal.fits");
+    // checkGLerror("after load_fits");
 
 
     //Add the healpix map to the scene
-    viewer.objects.push_back(img);
+    viewer.objects.push_back(hmap);
    	viewer.runLoop();
 
     // Terminate GLFW, clearing any resources allocated by GLFW.
